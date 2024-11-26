62b921a8b8709b97f6efe75c85e4a22e50201651
iTuanAnh <107922186+iTuanAnh@users.noreply.github.com>
2024-11-26 17:00:41 +0700

    feat(Kế hoạch lịch làm việc): khai báo thêm bảng plv_KeHoachLichLamViec

---
+++ ECP_V2.DataAccess/plv_KeHoachLichLamViec.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ECP_V2.DataAccess
+{
+    public class plv_KeHoachLichLamViec
+    {
+        public int Id { get; set; }
+
+        public int PhienLamViecId { get; set; }
+        public int HinhThucKiemTra { get; set; }
+
+        public string NguoiDaiDienKT_Id { get; set; }
+        public string NguoiDaiDienKT { get; set; }
+        public string LyDoHoanHuy { get; set; }
+
+
+
+    }
+}
